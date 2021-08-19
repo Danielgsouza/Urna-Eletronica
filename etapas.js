49e04c85ca308a297201147673a00a4b8a219ad9
Daniel Souza <danielgsouza4@gmail.com>
2021-08-18 22:26:02 -0300

    Finalização da Urna Eletrônica

---
+++ etapas.js
@@ -0,0 +1,69 @@
+let etapas = [ 
+    {
+    titulo: 'VEREADOR',
+    numeros: 5,
+    candidatos: [
+        {
+            numero: '10800',
+            nome:'Fulano de Tal',
+            partido:'ABC',
+            fotos:[
+                {url:'10800.png', legenda:'Vereador', small: false}
+            ]
+        },
+        {
+            numero: '22650',
+            nome:'Beltrano da Silva',
+            partido: 'DEFG',
+            fotos:[
+                {url:'22650.png', legenda:'Vereador'}
+            ]
+        },
+        {
+            numero: '38100',
+            nome:'Zezinho da Van',
+            partido:'HIJKL',
+            fotos:[
+                {url:'38100.png', legenda:'Vereador'}
+            ]
+        },
+    ]
+},
+{
+    titulo:'PREFEITO',
+    numeros: 2,
+    candidatos: [
+        {
+            numero: '10',
+            nome:'Antônio da Cocada',
+            partido:'ABC',
+            vice:'Cic',
+            fotos:[
+                {url:'10.png', legenda:'Prefeito'},
+                {url:'10.2.png', legenda:'Vice-Prefeito', small: true}
+            ]
+        },
+        {
+            numero: '22',
+            nome:'Pedro do Tâxi',
+            partido:'DEFG',
+            vice:'Mendoça da Pastelaria',
+            fotos:[
+                {url:'22.png', legenda:'Prefeito'},
+                {url:'22.2.png', legenda:'Vice-Prefeito', small: true}
+            ]
+        },
+        {
+            numero: '38',
+            nome:'claudio da Farmacia',
+            partido:'HIJKL',
+            vice:'Junior',
+            fotos:[
+                {url:'38.png', legenda:'Prefeito'},
+                {url:'38.2.png', legenda:'Vice-Prefeito'}
+            ]
+        },
+    ] 
+}
+];
+ 
